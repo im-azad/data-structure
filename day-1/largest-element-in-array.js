231625d445f5b98afd5ceb574f9c21e421fd8098
Abul Kalam Azad <48712125+im-azad@users.noreply.github.com>
2025-07-07 15:19:05 +0000

    Add function to find the largest element in an array

---
+++ day-1/largest-element-in-array.js
@@ -0,0 +1,15 @@
+function largest(arr) {
+    let max = arr[0];
+
+    // Traverse from second and compare
+    // every element with current max
+    for (let i = 1; i < arr.length; i++)
+        if (arr[i] > max)
+            max = arr[i];
+
+    return max;
+}
+
+// Driver Code
+const arr = [10, 324, 45, 90,100];
+console.log(largest(arr));
